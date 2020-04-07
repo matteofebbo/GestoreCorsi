534d7138744ade0f19eb86e32afb24e860a17129
Matteo Febbo <Matteo Febbo@DESKTOP-MQR87MO.homenet.telecomitalia.it>
2020-04-07 10:54:04 +0200

    soluzione

--- src/main/java/it/polito/tdp/corsi/FXMLController.java
+++ src/main/java/it/polito/tdp/corsi/FXMLController.java
@@ -1,25 +1,160 @@
 package it.polito.tdp.corsi;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.corsi.model.Corso;
+import it.polito.tdp.corsi.model.Model;
+import it.polito.tdp.corsi.model.Studente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+	
+	private Model model;
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TextField txtPeriodo;
+
+    @FXML
+    private TextField txtCorso;
+
+    @FXML
+    private Button btnCorsiPerPeriodo;
+
+    @FXML
+    private Button btnStudenti;
+
+    @FXML
+    private Button btnNumeroStudenti;
+
+    @FXML
+    private Button btnDivisioneStudenti;
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    void corsiPerPeriodo(ActionEvent event) {
+    	
+    	txtRisultato.clear();
+    	
+    	String pdString= txtPeriodo.getText();
+    	Integer pd;
+    	try {
+    		pd=Integer.parseInt(pdString);
+    	} catch (NumberFormatException e) {
+    		txtRisultato.setText("Devi inserire un numero ( 1 o 2 )");
+    		return;
+    	}
+    	
+    	if(!pd.equals(1) && !pd.equals(2)) {
+    		txtRisultato.setText("Devi inserire un numero ( 1 o 2 )");
+    		return;
+    	}
+    	
+    	// se arriviamo a questo punto l' imput e' corretto
+    	List<Corso> corsi= this.model.getCorsiByPeriodo(pd);
+    	for(Corso c : corsi) {
+    		txtRisultato.appendText(c.toString() + "\n");
+    	}
+    }
+
+    @FXML
+    void numeroStudenti(ActionEvent event) {
+    	txtRisultato.clear();
+    	
+    	String pdString= txtPeriodo.getText();
+    	Integer pd;
+    	try {
+    		pd=Integer.parseInt(pdString);
+    	} catch (NumberFormatException e) {
+    		txtRisultato.setText("Devi inserire un numero ( 1 o 2 )");
+    		return;
+    	}
+    	
+    	if(!pd.equals(1) && !pd.equals(2)) {
+    		txtRisultato.setText("Devi inserire un numero ( 1 o 2 )");
+    		return;
+    	}
+    	
+    	// se arriviamo a questo punto l' imput e' corretto
+    	Map<Corso,Integer> statistiche=this.model.getIscrittiByPeriodo(pd);
+    	for(Corso c:statistiche.keySet()) {
+    		txtRisultato.appendText(c.getNome() + " " +statistiche.get(c) + "\n");
+    	}
+    }
+
+    @FXML
+    void stampaDivisione(ActionEvent event) {
+txtRisultato.clear();
+    	
+    	String codins= txtCorso.getText();
+    	
+    	//controllare se il codice corrisponde a un corso esistente
+    	
+    	if(!model.esisteCorso(codins)) {
+    		txtRisultato.appendText("Corso insesistente\n");
+    		return;
+    	}
+    	
+    	Map<String,Integer> statistiche=this.model.getDivisioneCDS(new Corso(codins,null,null,null));
+    	for(String cds: statistiche.keySet()) {
+    		txtRisultato.appendText(cds+" "+ statistiche.get(cds)+"\n");
+    	}
+    }
+
+    @FXML
+    void stampaStudenti(ActionEvent event) {
+    	
+    	txtRisultato.clear();
+    	
+    	String codins= txtCorso.getText();
+    	
+    	//controllare se il codice corrisponde a un corso esistente
+    	
+    	if(!model.esisteCorso(codins)) {
+    		txtRisultato.appendText("Corso insesistente\n");
+    		return;
+    	}
+    	
+    	List<Studente> studenti= model.getStudentiByCorso(new Corso(codins,null,null,null));
+    	
+    	if(studenti.size()==0) {
+    		txtRisultato.appendText("il corso non ha studenti iscritti");
+    		return;
+    	}
+    	
+    	for (Studente s: studenti) {
+    		txtRisultato.appendText(s.toString()+"\n");
+    	}
+    	
+    }
+
+    @FXML
+    void initialize() {
+        assert txtPeriodo != null : "fx:id=\"txtPeriodo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCorso != null : "fx:id=\"txtCorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCorsiPerPeriodo != null : "fx:id=\"btnCorsiPerPeriodo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnStudenti != null : "fx:id=\"btnStudenti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnNumeroStudenti != null : "fx:id=\"btnNumeroStudenti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnDivisioneStudenti != null : "fx:id=\"btnDivisioneStudenti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Model model) {
+    	this.model=model;
+    }
 }
---
+++ src/main/java/it/polito/tdp/corsi/db/ConnectDB.java
@@ -0,0 +1,16 @@
+package it.polito.tdp.corsi.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class ConnectDB {
+	
+	public static Connection getConnection() throws SQLException {
+		String jdbcURL = "jdbc:mysql://localhost/iscritticorsi?user=root&password=milito99";
+		return DriverManager.getConnection(jdbcURL);
+	}
+}
